2c42cc897a1a407803b29fa36042246475e1ab54
ds4e <trj2j@virginia.edu>
2026-01-27 10:39:38 -0500

    Changes for recording

--- py_20260127.py
+++ py_20260127.py
@@ -1,47 +1,21 @@
 # %%
 
-import numpy as np 
-import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('./data/nhanes_data_17_18.csv')
+df = pd.read_csv('./data/craiglist_cville_cars_long.csv')
 df.head()
+ 
+# %%
 
-sns.kdeplot( df['CarbohydrateGm_DR1TOT'] )
-plt.show()
+sns.histplot( np.log( df['price']), bins = 50 )
 
-sns.ecdfplot(df['CarbohydrateGm_DR1TOT'])
-plt.show()
-
-x = df['CarbohydrateGm_DR1TOT'].dropna()
-iqr = np.quantile( x, .75) - np.quantile( x, .25)
-
-sns.boxplot( x = x )
-plt.show()
+df['price'].describe()
 
 # %%
 
-def outlier_analyze(x):
-    iqr = np.quantile( x, .75) - np.quantile( x, .25)
-    uw = np.quantile( x, .75) + 1.5 * iqr # upper whisker
-    lw = np.quantile( x, .25) - 1.5 * iqr # lower whisker
-    upper_outlier = ( x > uw ).astype(int)
-    lower_outlier = ( x < lw ).astype(int)
-    outlier = upper_outlier + lower_outlier
-    winsorize = ( upper_outlier * uw \
-                 + lower_outlier * lw \
-                 + (1-outlier) * x )
-    return outlier, winsorize
+sns.ecdfplot( df['price']) 
 
-
-outlier, winsorize = outlier_analyze(x)
 # %%
-
-sns.scatterplot( x=x, y=winsorize)
-# %%
-
-sns.violinplot(x=x,inner='quart',fill=False)
-# %%
-
-print('Work completed, things have changed!')
